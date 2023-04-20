07e35df60de62356e3a3af984db873cbb5778343
Marvin Lindner <mrvnlindner@gmail.com>
2023-04-20 15:54:00 +0200

    Initial App

---
+++ Productive/Productive/ProductiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductiveApp.swift
+//  Productive
+//
+//  Created by Lindner, Marvin on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ProductiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
