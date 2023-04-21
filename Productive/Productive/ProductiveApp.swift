6d9a63a9b47062f7f91fa0e0baa0a71c109a94bd
Marvin Lindner <mrvnlindner@gmail.com>
2023-04-21 11:51:45 +0200

    Added Firebase for backend

--- Productive/Productive/ProductiveApp.swift
+++ Productive/Productive/ProductiveApp.swift
@@ -6,6 +6,17 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ProductiveApp: App {
